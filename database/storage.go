16df9bb17ad048ebb155ebd62e32b590d81e4817
Igorok <fdiii@mail.ru>
2021-06-21 20:19:34 +0300

    feat: add tests

--- database/storage.go
+++ database/storage.go
@@ -1,9 +1,16 @@
 package database
 
+import "errors"
+
 var urls = make(map[string]string)
 
 func GetValue(key string) (string, error) {
-	return urls[key], nil
+	val, ok := urls[key]
+	if !ok {
+		return val, errors.New("this key doesn't exist")
+	}
+
+	return val, nil
 }
 
 func SetValue(key string, value string) {
